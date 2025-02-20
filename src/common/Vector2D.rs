89766b63d26847e64ced60d6d51f68118f96ec02
Erich Querner <querner@interactive-scape.com>
2025-02-20 15:09:40 +0100

    start implementing simple vector2 math

---
+++ src/common/Vector2D.rs
@@ -0,0 +1,22 @@
+﻿use std::ops;
+
+pub struct Vector2D {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl ops::Add<Vector2D> for Vector2D{
+    type Output = Vector2D;
+    fn add(self, _rhs: Vector2D) -> Vector2D{
+        Vector2D::new(self.x + _rhs.x, self.y + _rhs.y)
+    }
+}
+
+impl Vector2D{
+    fn new(x: f32, y: f32)->Vector2D{
+        Vector2D{
+            x,
+            y
+        }
+    }
+}
